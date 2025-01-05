8dc22f2b010246aa27ee7e4d31f31d2025a5b764
Kevin Bi <keveloper@gmail.com>
2025-01-05 12:52:03 -0600

    Fix warnings

--- src/components/SocialBar.js
+++ src/components/SocialBar.js
@@ -1,34 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const social_links = [
-    {
-        text: "GitHub",
-        url: "https://github.com/TheKeveloper"
-    },
-    {
-        text: "Email",
-        url: "mailto:kevinbi@college.harvard.edu"
-    },
-    {
-        text: "LinkedIn",
-        url: "https://www.linkedin.com/in/kevin-bi-006426148/"
-    },
-    {
-        text: "StackOverflow",
-        url: "https://stackoverflow.com/users/8371811/keveloper"
-    }
-]
+  {
+    text: "GitHub",
+    url: "https://github.com/TheKeveloper",
+  },
+  {
+    text: "Email",
+    url: "mailto:kevinbi@college.harvard.edu",
+  },
+  {
+    text: "LinkedIn",
+    url: "https://www.linkedin.com/in/kevin-bi-006426148/",
+  },
+  {
+    text: "StackOverflow",
+    url: "https://stackoverflow.com/users/8371811/keveloper",
+  },
+];
 export class SocialBar extends React.PureComponent {
-
-    render() {
-        return(<div style = {{textAlign: "center", marginBottom: 10}}>
-            {social_links.map(link => <a href = {link.url}
-                style = {{ 
-                    marginLeft: 20,
-                    marginRight: 20,
-                    fontSize: "14pt",
-                }}
-            target = "_blank"><strong>{link.text}</strong></a>)}
-        </div>)
-    }
+  render() {
+    return (
+      <div style={{ textAlign: "center", marginBottom: 10 }}>
+        {social_links.map((link) => (
+          <a
+            href={link.url}
+            style={{
+              marginLeft: 20,
+              marginRight: 20,
+              fontSize: "14pt",
+            }}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <strong>{link.text}</strong>
+          </a>
+        ))}
+      </div>
+    );
+  }
 }
