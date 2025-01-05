6604f92545655773e13122f72a336a10b5f7b870
TheKeveloper <TheKeveloper@users.noreply.github.com>
2025-01-05 19:09:46 +0000

    deploy: c540b619736143004fde6ed7dd19dd8aba306c87

---
+++ precache-manifest.4585240b8a748f70f7e9cfe68efdf5d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9999e40893cf1f1714fa3a0d6f9efbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8ace94556636a5b60ba",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "462e91eb2e6b4cb13b11",
+    "url": "/static/css/main.64e449f8.chunk.css"
+  },
+  {
+    "revision": "b8ace94556636a5b60ba",
+    "url": "/static/js/2.1f812505.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1f812505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462e91eb2e6b4cb13b11",
+    "url": "/static/js/main.8539e4a7.chunk.js"
+  },
+  {
+    "revision": "cc9ba3bdff8262ccea92",
+    "url": "/static/js/runtime-main.3424fe3d.js"
+  }
+]);
